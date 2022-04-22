90bb366363638c887e17f9d373c7715b8245901b
MyPasswordIsWeak <MyPasswordIsWeak@protonmail.com>
2022-04-22 22:33:56 +0200

    improve u128 api a little

--- include/nnc/u128.h
+++ include/nnc/u128.h
@@ -17,14 +17,14 @@ typedef struct nnc_u128 {
 #define NNC_HEX128(hi, lo) \
 	((nnc_u128) { hi, 0x##lo })
 
-/** Xor a with b saving the result in a. */
-void nnc_u128_xor(nnc_u128 *a, const nnc_u128 *b);
-/** Add a with b saving the result in a. */
-void nnc_u128_add(nnc_u128 *a, const nnc_u128 *b);
-/** Rotate a n bits to the right saving the result in a, n is at max 63. */
-void nnc_u128_rol(nnc_u128 *a, nnc_u8 n);
-/** Rotate a n bits to the left saving the result in a, n is at max 63. */
-void nnc_u128_ror(nnc_u128 *a, nnc_u8 n);
+/** Xor a with b saving the result in a and returning the pointer. */
+nnc_u128 *nnc_u128_xor(nnc_u128 *a, const nnc_u128 *b);
+/** Add a with b saving the result in a and returning the pointer. */
+nnc_u128 *nnc_u128_add(nnc_u128 *a, const nnc_u128 *b);
+/** Rotate a n bits to the right saving the result in a and returning the pointer, n is at max 63. */
+nnc_u128 *nnc_u128_rol(nnc_u128 *a, nnc_u8 n);
+/** Rotate a n bits to the left saving the result in a and returning the pointer, n is at max 63. */
+nnc_u128 *nnc_u128_ror(nnc_u128 *a, nnc_u8 n);
 /** Export the u128 as big-endian bytes */
 void nnc_u128_bytes(const nnc_u128 *a, nnc_u8 bytes[0x10]);
 
--- source/u128.c
+++ source/u128.c
@@ -5,13 +5,14 @@
 #include "./internal.h"
 
 
-void nnc_u128_xor(u128 *a, const u128 *b)
+u128 *nnc_u128_xor(u128 *a, const u128 *b)
 {
 	a->hi ^= b->hi;
 	a->lo ^= b->lo;
+	return a;
 }
 
-void nnc_u128_rol(u128 *a, u8 n)
+u128 *nnc_u128_rol(u128 *a, u8 n)
 {
 	if(n < 64)
 	{
@@ -24,12 +25,13 @@ void nnc_u128_rol(u128 *a, u8 n)
 	}
 	else
 	{
-		fprintf(stderr, "EINVAL: rol: n > 64 (n=%i)\n", n);
+		fprintf(stderr, "EINVAL: rol: n >= 64 (n=%i)\n", n);
 		abort();
 	}
+	return a;
 }
 
-void nnc_u128_ror(u128 *a, u8 n)
+u128 *nnc_u128_ror(u128 *a, u8 n)
 {
 	if(n < 64)
 	{
@@ -42,12 +44,13 @@ void nnc_u128_ror(u128 *a, u8 n)
 	}
 	else
 	{
-		fprintf(stderr, "EINVAL: ror: n > 64 (n=%i)\n", n);
+		fprintf(stderr, "EINVAL: ror: n >= 64 (n=%i)\n", n);
 		abort();
 	}
+	return a;
 }
 
-void nnc_u128_add(u128 *a, const u128 *b)
+u128 *nnc_u128_add(u128 *a, const u128 *b)
 {
 	u64 carry;
 	u64 add = a->lo + b->lo;
@@ -63,6 +66,7 @@ void nnc_u128_add(u128 *a, const u128 *b)
 		carry = 0;
 	}
 	a->hi += b->hi + carry;
+	return a;
 }
 
 void nnc_u128_bytes(const u128 *a, u8 bytes[0x10])
