80bc67668acabbb575f730e4eed09bb117af55c7
MyPasswordIsWeak <MyPasswordIsWeak@protonmail.com>
2023-07-18 21:39:36 +0200

    fix image swizzle functions

--- include/nnc/swizzle.h
+++ include/nnc/swizzle.h
@@ -13,7 +13,7 @@ NNC_BEGIN
  *  \param x     Image width, must be aligned by 8.
  *  \param y     Image height, must be aligned by 8.
  */
-void nnc_unswizzle_zorder_le_rgb565_to_rgba8(nnc_u16 *inp, nnc_u32 *outp, nnc_u8 x, nnc_u8 y);
+void nnc_unswizzle_zorder_le_rgb565_to_rgba8(nnc_u16 *inp, nnc_u32 *outp, nnc_u16 x, nnc_u16 y);
 
 /** \brief       Unswizzles a z-order curve rgb565 (little endian) image to an abgr8 (native endian) image.
  *  \param inp   Input array, should be at least of size `dim*dim*sizeof(nnc_u16)`.
@@ -21,7 +21,7 @@ void nnc_unswizzle_zorder_le_rgb565_to_rgba8(nnc_u16 *inp, nnc_u32 *outp, nnc_u8
  *  \param x     Image width, must be aligned by 8.
  *  \param y     Image height, must be aligned by 8.
  */
-void nnc_unswizzle_zorder_le_rgb565_to_abgr8(nnc_u16 *inp, nnc_u32 *outp, nnc_u8 x, nnc_u8 y);
+void nnc_unswizzle_zorder_le_rgb565_to_abgr8(nnc_u16 *inp, nnc_u32 *outp, nnc_u16 x, nnc_u16 y);
 
 /** \brief       Swizzles an rgba8 (native endian) image to a z-order curve rgb565 (little endian) image.
  *  \param inp   Input array, should be at least of size `dim*dim*sizeof(nnc_u32)`.
@@ -29,7 +29,7 @@ void nnc_unswizzle_zorder_le_rgb565_to_abgr8(nnc_u16 *inp, nnc_u32 *outp, nnc_u8
  *  \param x     Image width, must be aligned by 8.
  *  \param y     Image height, must be aligned by 8.
  */
-void nnc_swizzle_zorder_rgba8_to_le_rgb565(nnc_u32 *inp, nnc_u16 *outp, nnc_u8 x, nnc_u8 y);
+void nnc_swizzle_zorder_rgba8_to_le_rgb565(nnc_u32 *inp, nnc_u16 *outp, nnc_u16 x, nnc_u16 y);
 
 /** \brief       Swizzles an abgr8 (native endian) image to a z-order curve rgb565 (little endian) image.
  *  \param inp   Input array, should be at least of size `dim*dim*sizeof(nnc_u32)`.
@@ -37,7 +37,7 @@ void nnc_swizzle_zorder_rgba8_to_le_rgb565(nnc_u32 *inp, nnc_u16 *outp, nnc_u8 x
  *  \param x     Image width, must be aligned by 8.
  *  \param y     Image height, must be aligned by 8.
  */
-void nnc_swizzle_zorder_abgr8_to_le_rgb565(nnc_u32 *inp, nnc_u16 *outp, nnc_u8 x, nnc_u8 y);
+void nnc_swizzle_zorder_abgr8_to_le_rgb565(nnc_u32 *inp, nnc_u16 *outp, nnc_u16 x, nnc_u16 y);
 
 NNC_END
 #endif
--- source/swizzle.c
+++ source/swizzle.c
@@ -11,9 +11,9 @@
 	| SHIFT_BIT_TO(var, 2, 4 + offset)
 
 #define DEFINE_UNSWIZZLE_FUNC(format_name, inp_type, outp_type, endian_func, vars, conversion_routine)  \
-  void nnc_unswizzle_zorder_##format_name(inp_type *inp, outp_type *outp, nnc_u8 xdim, nnc_u8 ydim)     \
+  void nnc_unswizzle_zorder_##format_name(inp_type *inp, outp_type *outp, u16 xdim, u16 ydim)           \
   {                                                                                                     \
-    u8 sx, sy, x, y, block;                                                                             \
+    u16 sx, sy, x, y, block;                                                                            \
     inp_type in_colour;                                                                                 \
     outp_type out_colour;                                                                               \
     u32 j = 0;                                                                                          \
@@ -39,14 +39,14 @@
           sy = UNPACK_U3_SPREAD(block, 1);                                                              \
           in_colour = endian_func(inp[j++]);                                                            \
           conversion_routine                                                                            \
-          outp[(y + sy) * ydim + x + sx] = out_colour;                                                  \
+          outp[(y + sy) * xdim + x + sx] = out_colour;                                                  \
         }                                                                                               \
   }
 
 #define DEFINE_SWIZZLE_FUNC(format_name, inp_type, outp_type, endian_func, vars, conversion_routine)  \
-  void nnc_swizzle_zorder_##format_name(u32 *inp, u16 *outp, u8 xdim, u8 ydim)                        \
+  void nnc_swizzle_zorder_##format_name(u32 *inp, u16 *outp, u16 xdim, u16 ydim)                      \
   {                                                                                                   \
-    u8 sx, sy, y, x, block;                                                                           \
+    u16 sx, sy, y, x, block;                                                                          \
     inp_type in_colour;                                                                               \
     outp_type out_colour;                                                                             \
     u32 j = 0;                                                                                        \
@@ -61,7 +61,7 @@
         {                                                                                             \
           sx = UNPACK_U3_SPREAD(block, 0);                                                            \
           sy = UNPACK_U3_SPREAD(block, 1);                                                            \
-          in_colour = inp[(y + sy) * ydim + x + sx];                                                  \
+          in_colour = inp[(y + sy) * xdim + x + sx];                                                  \
           conversion_routine                                                                          \
           outp[j++] = endian_func(out_colour);                                                        \
         }                                                                                             \
@@ -77,9 +77,9 @@ DEFINE_UNSWIZZLE_FUNC(le_rgb565_to_rgba8, u16, u32, LE16,
 	u8 b;
 	u8 a = 0xFF; /* since there is no α channel encoded always set it to max */
 ,
-	r = FIX_COLOUR_DEPTH(in_colour >> 0  & 0x1F, 5, 8);
+	r = FIX_COLOUR_DEPTH(in_colour >> 11 & 0x1F, 5, 8);
 	g = FIX_COLOUR_DEPTH(in_colour >> 5  & 0x3F, 6, 8);
-	b = FIX_COLOUR_DEPTH(in_colour >> 11 & 0x1F, 5, 8);
+	b = FIX_COLOUR_DEPTH(in_colour >> 0  & 0x1F, 5, 8);
 	out_colour = (r << 0) | (g << 8) | (b << 16) | (a << 24);
 )
 
@@ -88,9 +88,9 @@ DEFINE_SWIZZLE_FUNC(rgba8_to_le_rgb565, u32, u16, LE16,
 	u8 g;
 	u8 b;
 ,
-	r = FIX_COLOUR_DEPTH(in_colour >> 0  & 0xFF, 8, 5);
-	g = FIX_COLOUR_DEPTH(in_colour >> 8  & 0xFF, 8, 6);
-	b = FIX_COLOUR_DEPTH(in_colour >> 16 & 0xFF, 8, 5);
+	r = FIX_COLOUR_DEPTH(in_colour >> 24 & 0xFF, 8, 5);
+	g = FIX_COLOUR_DEPTH(in_colour >> 16 & 0xFF, 8, 6);
+	b = FIX_COLOUR_DEPTH(in_colour >> 8  & 0xFF, 8, 5);
 	out_colour = (r << 0) | (g << 5) | (b << 11);
 )
 
@@ -100,9 +100,9 @@ DEFINE_UNSWIZZLE_FUNC(le_rgb565_to_abgr8, u16, u32, LE16,
 	u8 b;
 	u8 a = 0xFF; /* since there is no α channel encoded always set it to max */
 ,
-	r = FIX_COLOUR_DEPTH(in_colour >> 0  & 0x1F, 5, 8);
+	r = FIX_COLOUR_DEPTH(in_colour >> 11 & 0x1F, 5, 8);
 	g = FIX_COLOUR_DEPTH(in_colour >> 5  & 0x3F, 6, 8);
-	b = FIX_COLOUR_DEPTH(in_colour >> 11 & 0x1F, 5, 8);
+	b = FIX_COLOUR_DEPTH(in_colour >> 0  & 0x1F, 5, 8);
 	out_colour = (a << 0) | (b << 8) | (g << 16) | (r << 24);
 )
 
@@ -111,9 +111,9 @@ DEFINE_SWIZZLE_FUNC(abgr8_to_le_rgb565, u32, u16, LE16,
 	u8 g;
 	u8 b;
 ,
-	r = FIX_COLOUR_DEPTH(in_colour >> 16 & 0xFF, 8, 5);
-	g = FIX_COLOUR_DEPTH(in_colour >> 8  & 0xFF, 8, 6);
-	b = FIX_COLOUR_DEPTH(in_colour >> 0  & 0xFF, 8, 5);
+	r = FIX_COLOUR_DEPTH(in_colour >> 24 & 0xFF, 8, 5);
+	g = FIX_COLOUR_DEPTH(in_colour >> 16 & 0xFF, 8, 6);
+	b = FIX_COLOUR_DEPTH(in_colour >> 8  & 0xFF, 8, 5);
 	out_colour = (r << 0) | (g << 5) | (b << 11);
 )
 
