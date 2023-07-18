881d4a292f3559bba3f053c8cf6874137eb84869
MyPasswordIsWeak <MyPasswordIsWeak@protonmail.com>
2023-07-18 23:46:35 +0200

    more work on image swizzle functions

--- include/nnc/swizzle.h
+++ include/nnc/swizzle.h
@@ -15,13 +15,22 @@ NNC_BEGIN
  */
 void nnc_unswizzle_zorder_le_rgb565_to_rgba8(nnc_u16 *inp, nnc_u32 *outp, nnc_u16 x, nnc_u16 y);
 
-/** \brief       Unswizzles a z-order curve rgb565 (little endian) image to an abgr8 (native endian) image.
+/** \brief       Unswizzles a z-order curve rgb565 (little endian) image to an rgb8 image.
  *  \param inp   Input array, should be at least of size `dim*dim*sizeof(nnc_u16)`.
- *  \param outp  Output array, should be at least of size `dim*dim*sizeof(nnc_u32)`.
+ *  \param outp  Output array, should be at least of size `dim*dim*3*sizeof(nnc_u8)`.
  *  \param x     Image width, must be aligned by 8.
  *  \param y     Image height, must be aligned by 8.
+ *  \note        The output image data can be represented by the following struct:
+ *               \code
+ *               struct rgb8 {
+ *                 u8 r;
+ *                 u8 g;
+ *                 u8 b;
+ *               } __attribute__((packed));
+ *               \endcode
+ *  \warning     Due to alignment issues this format is not recommended.
  */
-void nnc_unswizzle_zorder_le_rgb565_to_abgr8(nnc_u16 *inp, nnc_u32 *outp, nnc_u16 x, nnc_u16 y);
+void nnc_unswizzle_zorder_le_rgb565_to_rgb8(nnc_u16 *inp, nnc_u8 *outp, nnc_u16 x, nnc_u16 y);
 
 /** \brief       Swizzles an rgba8 (native endian) image to a z-order curve rgb565 (little endian) image.
  *  \param inp   Input array, should be at least of size `dim*dim*sizeof(nnc_u32)`.
@@ -31,13 +40,22 @@ void nnc_unswizzle_zorder_le_rgb565_to_abgr8(nnc_u16 *inp, nnc_u32 *outp, nnc_u1
  */
 void nnc_swizzle_zorder_rgba8_to_le_rgb565(nnc_u32 *inp, nnc_u16 *outp, nnc_u16 x, nnc_u16 y);
 
-/** \brief       Swizzles an abgr8 (native endian) image to a z-order curve rgb565 (little endian) image.
+/** \brief       Swizzles an rgb8 image to a z-order curve rgb565 (little endian) image.
  *  \param inp   Input array, should be at least of size `dim*dim*sizeof(nnc_u32)`.
- *  \param outp  Output array, should be at least of size `dim*dim*sizeof(nnc_u16)`.
+ *  \param outp  Output array, should be at least of size `dim*dim*3*sizeof(nnc_u8)`.
  *  \param x     Image width, must be aligned by 8.
  *  \param y     Image height, must be aligned by 8.
+ *  \note        The input image data can be represented by the following struct:
+ *               \code
+ *               struct rgb8 {
+ *                 u8 r;
+ *                 u8 g;
+ *                 u8 b;
+ *               } __attribute__((packed));
+ *               \endcode
+ *  \warning     Due to alignment issues this format is not recommended.
  */
-void nnc_swizzle_zorder_abgr8_to_le_rgb565(nnc_u32 *inp, nnc_u16 *outp, nnc_u16 x, nnc_u16 y);
+void nnc_swizzle_zorder_rgb8_to_le_rgb565(nnc_u8 *inp, nnc_u16 *outp, nnc_u16 x, nnc_u16 y);
 
 NNC_END
 #endif
--- source/swizzle.c
+++ source/swizzle.c
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include "./internal.h"
 
+/* This code is total chaos...... */
+
 /* N.B.: `from' must be less than `to'! */
 #define SHIFT_BIT_TO(var, to, from) (  ((var >> (from)) & 1) << (to)  )
 #define UNPACK_U3_SPREAD(var, offset) \
@@ -10,110 +12,115 @@
 	| SHIFT_BIT_TO(var, 1, 2 + offset) \
 	| SHIFT_BIT_TO(var, 2, 4 + offset)
 
-#define DEFINE_UNSWIZZLE_FUNC(format_name, inp_type, outp_type, endian_func, vars, conversion_routine)  \
-  void nnc_unswizzle_zorder_##format_name(inp_type *inp, outp_type *outp, u16 xdim, u16 ydim)           \
-  {                                                                                                     \
-    u16 sx, sy, x, y, block;                                                                            \
-    inp_type in_colour;                                                                                 \
-    outp_type out_colour;                                                                               \
-    u32 j = 0;                                                                                          \
-    vars                                                                                                \
-                                                                                                        \
-    /* `dim' must be aligned by 8 due to the blocks being 8x8,                                          \
-     * this assert is obviously suboptimal but for the sake                                             \
-     * of easier code it's better than returning a result */                                            \
-    assert(IS_ALIGNED(xdim, 8));                                                                        \
-    assert(IS_ALIGNED(ydim, 8));                                                                        \
-                                                                                                        \
-    /* Nintendo swizzles their images in block of 8x8, with those blocks                                \
-     *  containing the image data z-order swizzled                                                      \
-     * see: https://en.wikipedia.org/wiki/Z-order_curve */                                              \
-    for(y = 0; y < ydim; y += 8)                                                                        \
-      for(x = 0; x < xdim; x += 8)                                                                      \
-        for(block = 0; block < 8 * 8; ++block)                                                          \
-        {                                                                                               \
-          /* The range for these is small enough to make a table                                        \
-           * but it doesn't pose a large performance problem (afaik)                                    \
-           * so i'll just keep it like this for simplicity */                                           \
-          sx = UNPACK_U3_SPREAD(block, 0);                                                              \
-          sy = UNPACK_U3_SPREAD(block, 1);                                                              \
-          in_colour = endian_func(inp[j++]);                                                            \
-          conversion_routine                                                                            \
-          outp[(y + sy) * xdim + x + sx] = out_colour;                                                  \
-        }                                                                                               \
+#define SWIZZLE_CUR_INDEX ((y + sy) * xdim + x + sx)
+
+#define DEFINE_UNSWIZZLE_FUNC_EX(format_name, inp_type, outp_type, endian_func, vars, conversion_routine)  \
+  void nnc_unswizzle_zorder_##format_name(inp_type *inp, outp_type *outp, u16 xdim, u16 ydim)              \
+  {                                                                                                        \
+    u16 sx, sy, x, y, block;                                                                               \
+    inp_type in_colour;                                                                                    \
+    u32 j = 0;                                                                                             \
+    vars                                                                                                   \
+                                                                                                           \
+    /* `dim' must be aligned by 8 due to the blocks being 8x8,                                             \
+     * this assert is obviously suboptimal but for the sake                                                \
+     * of easier code it's better than returning a result */                                               \
+    assert(IS_ALIGNED(xdim, 8));                                                                           \
+    assert(IS_ALIGNED(ydim, 8));                                                                           \
+                                                                                                           \
+    /* Nintendo swizzles their images in block of 8x8, with those blocks                                   \
+     *  containing the image data z-order swizzled                                                         \
+     * see: https://en.wikipedia.org/wiki/Z-order_curve */                                                 \
+    for(y = 0; y < ydim; y += 8)                                                                           \
+      for(x = 0; x < xdim; x += 8)                                                                         \
+        for(block = 0; block < 8 * 8; ++block)                                                             \
+        {                                                                                                  \
+          /* The range for these is small enough to make a table                                           \
+           * but it doesn't pose a large performance problem (afaik)                                       \
+           * so i'll just keep it like this for simplicity */                                              \
+          sx = UNPACK_U3_SPREAD(block, 0);                                                                 \
+          sy = UNPACK_U3_SPREAD(block, 1);                                                                 \
+          in_colour = endian_func(inp[j++]);                                                               \
+          conversion_routine                                                                               \
+        }                                                                                                  \
   }
 
-#define DEFINE_SWIZZLE_FUNC(format_name, inp_type, outp_type, endian_func, vars, conversion_routine)  \
-  void nnc_swizzle_zorder_##format_name(u32 *inp, u16 *outp, u16 xdim, u16 ydim)                      \
-  {                                                                                                   \
-    u16 sx, sy, y, x, block;                                                                          \
-    inp_type in_colour;                                                                               \
-    outp_type out_colour;                                                                             \
-    u32 j = 0;                                                                                        \
-    vars                                                                                              \
-                                                                                                      \
-    assert(IS_ALIGNED(xdim, 8));                                                                      \
-    assert(IS_ALIGNED(ydim, 8));                                                                      \
-                                                                                                      \
-    for(y = 0; y < ydim; y += 8)                                                                      \
-      for(x = 0; x < xdim; x += 8)                                                                    \
-        for(block = 0; block < 8 * 8; ++block)                                                        \
-        {                                                                                             \
-          sx = UNPACK_U3_SPREAD(block, 0);                                                            \
-          sy = UNPACK_U3_SPREAD(block, 1);                                                            \
-          in_colour = inp[(y + sy) * xdim + x + sx];                                                  \
-          conversion_routine                                                                          \
-          outp[j++] = endian_func(out_colour);                                                        \
-        }                                                                                             \
+#define DEFINE_UNSWIZZLE_FUNC(format_name, inp_type, outp_type, endian_func, vars, conversion_routine) \
+  DEFINE_UNSWIZZLE_FUNC_EX(format_name, inp_type, outp_type, endian_func, outp_type out_colour; vars, conversion_routine outp[SWIZZLE_CUR_INDEX] = out_colour;)
+
+#define DEFINE_SWIZZLE_FUNC_EX(format_name, inp_type, outp_type, endian_func, vars, conversion_routine)  \
+  void nnc_swizzle_zorder_##format_name(inp_type *inp, outp_type *outp, u16 xdim, u16 ydim)              \
+  {                                                                                                      \
+    u16 sx, sy, y, x, block;                                                                             \
+    outp_type out_colour;                                                                                \
+    u32 j = 0;                                                                                           \
+    vars                                                                                                 \
+                                                                                                         \
+    assert(IS_ALIGNED(xdim, 8));                                                                         \
+    assert(IS_ALIGNED(ydim, 8));                                                                         \
+                                                                                                         \
+    for(y = 0; y < ydim; y += 8)                                                                         \
+      for(x = 0; x < xdim; x += 8)                                                                       \
+        for(block = 0; block < 8 * 8; ++block)                                                           \
+        {                                                                                                \
+          sx = UNPACK_U3_SPREAD(block, 0);                                                               \
+          sy = UNPACK_U3_SPREAD(block, 1);                                                               \
+          conversion_routine                                                                             \
+          outp[j++] = endian_func(out_colour);                                                           \
+        }                                                                                                \
   }
 
+#define DEFINE_SWIZZLE_FUNC(format_name, inp_type, outp_type, endian_func, vars, conversion_routine) \
+  DEFINE_SWIZZLE_FUNC_EX(format_name, inp_type, outp_type, endian_func, inp_type in_colour; vars, in_colour = inp[SWIZZLE_CUR_INDEX]; conversion_routine)
 
 #define FIX_COLOUR_DEPTH(clr, in_bits, out_bits) \
 	( ( ((float) (clr)) / ( (float) ( (1 << in_bits) - 1) ) ) * ( (1 << out_bits) - 1 ) )
 
-DEFINE_UNSWIZZLE_FUNC(le_rgb565_to_rgba8, u16, u32, LE16,
-	u8 r;
-	u8 g;
-	u8 b;
-	u8 a = 0xFF; /* since there is no α channel encoded always set it to max */
-,
-	r = FIX_COLOUR_DEPTH(in_colour >> 11 & 0x1F, 5, 8);
-	g = FIX_COLOUR_DEPTH(in_colour >> 5  & 0x3F, 6, 8);
-	b = FIX_COLOUR_DEPTH(in_colour >> 0  & 0x1F, 5, 8);
-	out_colour = (r << 0) | (g << 8) | (b << 16) | (a << 24);
-)
+#define ALPHA_DO_NOTHING(x)
+#define ALPHA_DO_SOMETHING(x) x
 
-DEFINE_SWIZZLE_FUNC(rgba8_to_le_rgb565, u32, u16, LE16,
-	u8 r;
-	u8 g;
-	u8 b;
-,
-	r = FIX_COLOUR_DEPTH(in_colour >> 24 & 0xFF, 8, 5);
-	g = FIX_COLOUR_DEPTH(in_colour >> 16 & 0xFF, 8, 6);
-	b = FIX_COLOUR_DEPTH(in_colour >> 8  & 0xFF, 8, 5);
-	out_colour = (r << 0) | (g << 5) | (b << 11);
-)
+#define ALPHA_DISABLE() ALPHA_DO_NOTHING
+#define ALPHA_ENABLE() ALPHA_DO_SOMETHING
 
-DEFINE_UNSWIZZLE_FUNC(le_rgb565_to_abgr8, u16, u32, LE16,
-	u8 r;
-	u8 g;
-	u8 b;
-	u8 a = 0xFF; /* since there is no α channel encoded always set it to max */
-,
-	r = FIX_COLOUR_DEPTH(in_colour >> 11 & 0x1F, 5, 8);
-	g = FIX_COLOUR_DEPTH(in_colour >> 5  & 0x3F, 6, 8);
-	b = FIX_COLOUR_DEPTH(in_colour >> 0  & 0x1F, 5, 8);
-	out_colour = (a << 0) | (b << 8) | (g << 16) | (r << 24);
-)
+#define input_alpha_bitlen ALPHA_BITS
 
-DEFINE_SWIZZLE_FUNC(abgr8_to_le_rgb565, u32, u16, LE16,
-	u8 r;
-	u8 g;
-	u8 b;
-,
-	r = FIX_COLOUR_DEPTH(in_colour >> 24 & 0xFF, 8, 5);
-	g = FIX_COLOUR_DEPTH(in_colour >> 16 & 0xFF, 8, 6);
-	b = FIX_COLOUR_DEPTH(in_colour >> 8  & 0xFF, 8, 5);
-	out_colour = (r << 0) | (g << 5) | (b << 11);
+#define DEFINE_SWIZZLE_FUNC_PAIR(input_format_name, input_total_bitlen, input_red_bitlen, input_green_bitlen, input_blue_bitlen, do_with_alpha, unswizzle_vars, unswizzle_colours, swizzle_colours) \
+	DEFINE_UNSWIZZLE_FUNC(le_##input_format_name##_to_rgba8, u##input_total_bitlen, u32, LE##input_total_bitlen, unswizzle_vars,                                         \
+		unswizzle_colours                                                                                                                                                  \
+		out_colour = (r << 24) | (g << 16) | (b << 8) | (a << 0);                                                                                                          \
+	)                                                                                                                                                                    \
+	DEFINE_SWIZZLE_FUNC(rgba8_to_le_##input_format_name, u32, u##input_total_bitlen, LE##input_total_bitlen, u8 r; u8 g; u8 b; do_with_alpha(u8 a;),                     \
+		r = FIX_COLOUR_DEPTH(in_colour >> 24 & 0xFF, 8, input_red_bitlen);                                                                                                 \
+		g = FIX_COLOUR_DEPTH(in_colour >> 16 & 0xFF, 8, input_green_bitlen);                                                                                               \
+		b = FIX_COLOUR_DEPTH(in_colour >> 8  & 0xFF, 8, input_blue_bitlen);                                                                                                \
+		do_with_alpha(a = FIX_COLOUR_DEPTH(in_colour >> 0 & 0xFF, 8, input_alpha_bitlen);)                                                                                 \
+		swizzle_colours                                                                                                                                                    \
+	)                                                                                                                                                                    \
+	DEFINE_UNSWIZZLE_FUNC_EX(le_##input_format_name##_to_rgb8, u##input_total_bitlen, u8, LE##input_total_bitlen,                                                        \
+		unswizzle_vars                                                                                                                                                     \
+		u8 *current;                                                                                                                                                       \
+	,                                                                                                                                                                    \
+		unswizzle_colours                                                                                                                                                  \
+		current = &outp[3 * SWIZZLE_CUR_INDEX];                                                                                                                            \
+		current[0] = r;                                                                                                                                                    \
+		current[1] = g;                                                                                                                                                    \
+		current[2] = b;                                                                                                                                                    \
+		(void) a; /* a is stripped in this format */                                                                                                                       \
+	)                                                                                                                                                                    \
+	DEFINE_SWIZZLE_FUNC_EX(rgb8_to_le_##input_format_name, u8, u##input_total_bitlen, LE##input_total_bitlen, u8 r; u8 g; u8 b; do_with_alpha(u8 a = 0xFF) u8 *current;, \
+		current = &inp[3 * SWIZZLE_CUR_INDEX];                                                                                                                             \
+		r = FIX_COLOUR_DEPTH(current[0], 8, input_red_bitlen);                                                                                                             \
+		g = FIX_COLOUR_DEPTH(current[1], 8, input_green_bitlen);                                                                                                           \
+		b = FIX_COLOUR_DEPTH(current[2], 8, input_blue_bitlen);                                                                                                            \
+		/* we don't support alpha in this format, hardcode to 0xFF */                                                                                                      \
+		swizzle_colours                                                                                                                                                    \
+	)
+
+DEFINE_SWIZZLE_FUNC_PAIR(rgb565, /* total bits: */ 16, /* red bits: */ 5, /* green bits: */ 6, /* blue bits: */ 5, /* alpha component: */ ALPHA_DISABLE()
+	, /* unswizzle vars:    */ u8 r; u8 g; u8 b; u8 a = 0xFF;
+	, /* unswizzle colours: */ r = FIX_COLOUR_DEPTH(in_colour >> 11 & 0x1F, 5, 8);
+	                           g = FIX_COLOUR_DEPTH(in_colour >> 5  & 0x3F, 6, 8);
+	                           b = FIX_COLOUR_DEPTH(in_colour >> 0  & 0x1F, 5, 8);
+ 	, /* swizzle colours:   */ out_colour = (r << 11) | (g << 5) | (b << 0);
 )
 
